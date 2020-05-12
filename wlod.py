892fa322738160deedcfd50d3dd1791ee8274ee9
Wolodymyr <wolodyak@hotmail.co.uk>
2020-05-12 23:59:26 +0100

    hell yea

--- wlod.py
+++ wlod.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-train = np.array(pd.read_csv("../m5-forecasting-uncertainty/sales_train_validation.csv"))
-sell_prices = pd.read_csv("../m5-forecasting-uncertainty/sell_prices.csv")
+train = np.array(pd.read_csv("input/m5-forecasting-uncertainty/sales_train_validation.csv"))
+sell_prices = pd.read_csv("input/m5-forecasting-uncertainty/sell_prices.csv")
 print(train[2])
 
 for i in range(1,int(np.shape(train)/2)):
