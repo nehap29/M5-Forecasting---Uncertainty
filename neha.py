e5a5604438d6f24672c7394663c1f683aa5ab03f
Neha Patil <neha.patil@student.manchester.ac.uk>
2020-05-12 23:58:21 +0100

    fix

--- neha.py
+++ neha.py
@@ -4,11 +4,39 @@ import numpy as np                       # dense matrices
 from scipy.sparse import csr_matrix      # sparse matrices
 
 
-train = pd.read_csv("input/m5-forecasting-uncertainty/sales_train_validation.csv")
+train = pd.read_csv("m5-forecasting-uncertainty/sales_train_validation.csv")
 print(train.head())
 
 train.shape
 
 
-calendar = pd.read_csv("input/m5-forecasting-uncertainty/calendar.csv")
+calendar = pd.read_csv("m5-forecasting-uncertainty/calendar.csv")
 calendar.head()
+
+sell_prices = pd.read_csv("m5-forecasting-uncertainty/sell_prices.csv")
+sell_prices.head()
+
+
+series_cols = train.columns[train.columns.str.contains("d_")].values
+level_cols = train.columns[train.columns.str.contains("d_")==False].values
+
+train.head(1)
+
+sns.set_palette("colorblind")
+
+fig, ax = plt.subplots(5,1,figsize=(20,28))
+train[series_cols].sum().plot(ax=ax[0])
+ax[0].set_title("Top-Level-1: Summed product sales of all stores and states")
+ax[0].set_ylabel("Unit sales of all products");
+train.groupby("state_id")[series_cols].sum().transpose().plot(ax=ax[1])
+ax[1].set_title("Level-2: Summed product sales of all stores per state");
+ax[1].set_ylabel("Unit sales of all products");
+train.groupby("store_id")[series_cols].sum().transpose().plot(ax=ax[2])
+ax[2].set_title("Level-3: Summed product sales per store")
+ax[2].set_ylabel("Unit sales of all products");
+train.groupby("cat_id")[series_cols].sum().transpose().plot(ax=ax[3])
+ax[3].set_title("Level-4: Summed product sales per category")
+ax[3].set_ylabel("Unit sales of all products");
+train.groupby("dept_id")[series_cols].sum().transpose().plot(ax=ax[4])
+ax[4].set_title("Level-4: Summed product sales per product department")
+ax[4].set_ylabel("Unit sales of all products");
