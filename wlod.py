00169b41207c563c8422c5366a3d89eee2de467b
Wolodymyr <wolodyak@hotmail.co.uk>
2020-05-13 00:31:32 +0100

    fix path

--- wlod.py
+++ wlod.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-train = np.array(pd.read_csv(r'../input/sales_train_validation.csv'))
+train = np.array(pd.read_csv("C:/Users/wolod/Documents/Programming/Kaggle/uncertainty/input/sales_train_validation.csv"))
 
 plt.plot(np.arange(np.shape(train)[1] - 6), np.sum(train,axis=0)[6:])
 
