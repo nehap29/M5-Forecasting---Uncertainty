93130969d43f0b95eceef25e3fafa8d7b0a790e5
Wolodymyr <wolodyak@hotmail.co.uk>
2020-05-13 00:26:58 +0100

    a

--- wlod.py
+++ wlod.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-train = np.array(pd.read_csv("input/m5-forecasting-uncertainty/sales_train_validation.csv"))
-sell_prices = pd.read_csv("input/m5-forecasting-uncertainty/sell_prices.csv")
-print(train[2])
+train = np.array(pd.read_csv(r'../input/sales_train_validation.csv'))
 
-plt.plot(np.arange(np.shape(train)[1] - 6), np.cumsum(np.sum(train,axis=0)[6:]))
+plt.plot(np.arange(np.shape(train)[1] - 6), np.sum(train,axis=0)[6:])
 
 plt.show()
